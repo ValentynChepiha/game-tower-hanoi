ececdb4625274f47ad3fdd20e0b915c8d3655a4a
ValentynChepiha <valentin.chepiga@gmail.com>
2019-12-12 23:55:59 +0200

    Fix bug. Game work. Level easy

--- js/index.js
+++ js/index.js
@@ -25,12 +25,13 @@ const baseColor = '#984c0d';
 let level = 1;
 let score = 0;
 // масив для контролю руху блоків
-let bricksSolution = [[],[],[]];
+const bricksSolution = [[],[],[]];
+const bricksPrevious = [[],[],[]];
+const pillarSolution = [];
 let flagMove = false;
 let brickIndexMove = -1;
 
 const drawBrick = ({x=10, y=100, w=25 , h=30, c='#ff0091'}) => {
-// const drawBrick = ({x = 10, y = 100, w = 190, h = 25, color = '#ff0091'}) => {
   ctx.beginPath();
   ctx.fillStyle = c;
   ctx.fillRect(x, y, w, h);
@@ -56,20 +57,22 @@ const drawPillar = (x) => {
   ctx.closePath();
 };
 
+const drawPillars = () => {
+  for(let i = 0; i<3; i++){
+    drawPillar(pillarSolution[i]);
+  }
+};
+
 const minCountSteps = (n=1) => {
   return 2**(n*2+1)-1;
 };
 
 const getLevel = (l=1) => {
   switch (l) {
-    case 1:
-      return 'Easy';
-    case 2:
-      return 'Average';
-    case 3:
-      return 'Hard';
-    default:
-      return 'Unreal';
+    case 1: return 'Easy';
+    case 2: return 'Average';
+    case 3: return 'Hard';
+    default: return 'Unreal';
   }
 };
 
@@ -93,8 +96,10 @@ const drawLevel = () => {
 
 const initPillars = () => {
   for(let i = 0; i<3; i++){
-    drawPillar(pillarX + i*2*pillarX+i*pillarWidth);
+    pillarSolution.push(pillarX + i*2*pillarX+i*pillarWidth);
+    drawPillar(pillarSolution[i]);
   }
+  console.log('init',pillarSolution);
 };
 
 const initBricks = () => {
@@ -106,21 +111,24 @@ const initBricks = () => {
       w:bricksWidth[level-1][i-1],
       h:bricksHeight[level-1],
       c:bricksColor[i-1]};
-    bricksSolution[0].unshift(obj);
+    bricksSolution[0].unshift(Object.assign({}, obj));
+    bricksPrevious[0].unshift(Object.assign({}, obj));
     drawBrick(obj);
   }
 };
 
 const inBrick = (x, y, elm) => {
-  console.log('inBrinck', x, y, elm);
   return x>=elm.x && x<=elm.x+elm.w && y>=elm.y && y<=elm.y+elm.h;
 };
 
-const handlerMousedown = (e) => {
+const numberBetween = (x,a,b) => {
+  return x>=a && x<=b;
+};
+
+const handlerPointerdown = (e) => {
   if(e.which === 1){
     for(let i=0; i<3; i++){
-      console.log(e);
-      if(bricksSolution[i][0]!==undefined && inBrick(e.layerX, e.layerY, bricksSolution[i][0])){
+      if(bricksSolution[i][0]!==undefined && inBrick(e.clientX - e.target.offsetLeft, e.clientY - e.target.offsetTop, bricksSolution[i][0])){
         brickIndexMove = i;
         flagMove = true;
       }
@@ -128,37 +136,62 @@ const handlerMousedown = (e) => {
   }
 };
 
-const handlerMouseup = () => {
+const handlerPointerup = () => {
+  if(flagMove){
+    let idPillar = -1;
+    let b = bricksSolution[brickIndexMove][0];
+    for(let i=0; i<3; i++){
+      let pX = pillarSolution[i];
+      if((b.y + b.h >= canvas.height - pillarHeight) && (numberBetween(pX, b.x, (b.x+b.w)) || numberBetween((pX+pillarWidth), b.x, (b.x+b.w)))){
+        idPillar = i;
+      }
+    }
+    if(idPillar>=0 &&
+      (bricksSolution[idPillar].length===0 ||
+        (bricksSolution[idPillar].length>0 && bricksSolution[idPillar][0].w > bricksSolution[brickIndexMove][0].w))){
+      let newPosition = bricksSolution[brickIndexMove].shift();
+
+      newPosition.x = pillarX + pillarX * idPillar * 2 + pillarWidth * idPillar - (newPosition.w - pillarWidth)/2;
+      newPosition.y = (canvas.height - (bricksSolution[idPillar].length + 1)*bricksHeight[level-1]);
+
+      bricksSolution[idPillar].unshift(Object.assign({}, newPosition));
+
+      bricksPrevious[brickIndexMove].shift();
+      bricksPrevious[idPillar].unshift(Object.assign({}, newPosition));
+      score++;
+    } else {
+      bricksSolution[brickIndexMove][0] = Object.assign({}, bricksPrevious[brickIndexMove][0]);
+    }
+  }
   flagMove = false;
 };
 
-const handlerMousemove = (e) => {
+const handlerPointermove = (e) => {
   if(flagMove){
-    console.log(e);
     bricksSolution[brickIndexMove][0].x += e.movementX;
     bricksSolution[brickIndexMove][0].y += e.movementY;
   }
 };
 
 const draw = () => {
-  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.clearRect(0,0, canvas.width, canvas.height);
   if (level>3 || level<1) {
     return;
   }
-  initPillars();
+  drawPillars();
   drawBricks();
 
   drawScore();
   drawLevel();
   drawMinSteps();
+  requestAnimationFrame(draw);
 };
 
-document.addEventListener('mousedown', handlerMousedown, false);
-document.addEventListener('mouseup', handlerMouseup, false);
-document.addEventListener('mousemove', handlerMousemove, false);
+document.addEventListener('pointerdown', handlerPointerdown, false);
+document.addEventListener('pointerup', handlerPointerup, false);
+document.addEventListener('pointermove', handlerPointermove, false);
 
 initPillars();
 initBricks();
 
-setInterval(draw, 10);
-// draw();
+draw();
